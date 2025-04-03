df0e1292ca19d06080abd090a3300782547f687e
Jatin Malik <malikjatin881@gmail.com>
2025-04-03 16:15:11 +0530

    added type tuple

--- tuple.go
+++ tuple.go
@@ -1,5 +1,26 @@
 package yart
 
+// TODO: Why do we need to distinguish between point and a vector?
 type tuple struct {
 	x, y, z, w float64
+}
+
+func (t tuple) IsPoint() bool {
+	return t.w == 1.0
+}
+
+func (t tuple) IsVector() bool {
+	return t.w == 0.0
+}
+
+func Point(x, y, z float64) tuple {
+	return tuple{x, y, z, 1.0}
+}
+
+func Vector(x, y, z float64) tuple {
+	return tuple{x, y, z, 0.0}
+}
+
+func (t tuple) Equal(t2 tuple) bool {
+	return EqualFloat64(t.x, t2.x) && EqualFloat64(t.y, t2.y) && EqualFloat64(t.z, t2.z) && EqualFloat64(t.w, t2.w)
 }
---
+++ util.go
@@ -0,0 +1,11 @@
+package yart
+
+import "math"
+
+const EPSILON = 0.00001
+
+// TODO: Do we really need to have this utility? Does Golang does this reliably for us?
+
+func EqualFloat64(a, b float64) bool {
+	return math.Abs(a-b) < EPSILON
+}
